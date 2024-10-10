2af4bc14de4376c59079985117f7002b12317435
Arthur <okoloarthur4@gmail.com>
2024-10-10 03:59:40 +0100

    create endpoint to get all submissions and to approve a submission, refactor existing method and create method to submit answer, show leaderboard, award points, and approve answer. create endpoint for user to submit answer and view leaderboard

---
+++ apps/main/src/controllers/user-trivia-controller.ts
@@ -0,0 +1,83 @@
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ApiTags, ApiResponse, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { PageOptionsDto, PaginationResponseDto } from 'libs/dto/page.dto';
+import {
+  AnswerDto,
+  LeaderboardResponseDto,
+  TriviaResponseDto,
+} from 'libs/dto/trivia.dto';
+import { TriviaService } from 'modules/trivia/trivia.service';
+
+@ApiTags('User Trivia Manager')
+@Controller('user-trivia')
+@UseInterceptors(ClassSerializerInterceptor)
+export class UserTriviaController {
+  constructor(private readonly triviaService: TriviaService) {}
+
+  @ApiOperation({ summary: 'List all trivias' })
+  @Get('all')
+  @ApiResponse({
+    status: 200,
+    description: 'List of all trivias.',
+    type: [PaginationResponseDto],
+  })
+  async getAllTrivias(@Query() options: PageOptionsDto) {
+    return this.triviaService.getAllTrivias(options);
+  }
+
+  @ApiOperation({ summary: 'Get current Trivia details' })
+  @Get(':id/detail')
+  @ApiResponse({
+    status: 200,
+    description: 'Trivia found.',
+    type: TriviaResponseDto,
+  })
+  @ApiResponse({ status: 404, description: 'Trivia not found.' })
+  async getTriviaById(@Param('id') id: string) {
+    return this.triviaService.getTriviaById(id);
+  }
+
+  @ApiOperation({ summary: 'Submit trivia answer' })
+  @ApiParam({
+    name: 'triviaId',
+    required: true,
+    description: '',
+  })
+  @ApiParam({
+    name: 'userId',
+    required: true,
+    description: '',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Github repository link was submitted successfully',
+  })
+  @Post(':triviaId/submit/:userId')
+  async submitAnswer(
+    @Param('userId') userId: string,
+    @Param('triviaId') triviaId: string,
+    @Body() answerDto: AnswerDto,
+  ) {
+    return this.triviaService.submitTriviaAnswer(userId, triviaId, answerDto);
+  }
+
+  @ApiOperation({ summary: 'Show leaderboard' })
+  @Get('leaderboard')
+  @ApiResponse({
+    status: 200,
+    description: 'Leaderboard displayed successfully',
+    type: [LeaderboardResponseDto],
+  })
+  async showLeader() {
+    return this.triviaService.showLeaderboard();
+  }
+}
--- modules/trivia/trivia.service.ts
+++ modules/trivia/trivia.service.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   BadRequestException,
   ConflictException,
+  GoneException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -10,17 +12,41 @@ import {
   PageOptionsDto,
   PaginationResponseDto,
 } from 'libs/dto/page.dto';
-import { CreateTriviaDto, UpdateTriviaDto } from 'libs/dto/trivia.dto';
+import {
+  AnswerDto,
+  CreateTriviaDto,
+  LeaderboardResponseDto,
+  TriviaResponseDto,
+  UpdateTriviaDto,
+} from 'libs/dto/trivia.dto';
+import { ERROR } from 'libs/enums/error.enum';
 import { Order } from 'libs/enums/order.enum';
-import { toTriviaResponse } from 'libs/mapper/trivia.mapper';
+import {
+  REVIEW_STATUS,
+  SUBMISSION_STATUS,
+  TRIVIA_STATUS,
+} from 'libs/enums/status.enum';
+import {
+  toSubmissionResponse,
+  toTriviaResponse,
+} from 'libs/mapper/trivia.mapper';
+import { toLeaderBoard } from 'libs/mapper/user.mapper';
+import { Submission } from 'libs/schema/submission.schema';
 import { Trivia } from 'libs/schema/trivia.schema';
 import { createPageOptionFallBack } from 'libs/utils/createPageOptionFallBack';
+import { UserService } from 'modules/user/user.service';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class TriviaService {
   constructor(
-    @InjectModel(Trivia.name) private readonly triviaRepo: Model<Trivia>,
+    @InjectModel(Trivia.name)
+    private readonly triviaRepo: Model<Trivia>,
+
+    @InjectModel(Submission.name)
+    private readonly submissionRepo: Model<Submission>,
+
+    private readonly userService: UserService,
   ) {}
 
   async createTrivia(triviaDto: CreateTriviaDto) {
@@ -42,7 +68,9 @@ export class TriviaService {
     };
   }
 
-  async getAllTrivias(options: PageOptionsDto): Promise<PaginationResponseDto> {
+  async getAllTrivias(
+    options: PageOptionsDto,
+  ): Promise<PaginationResponseDto<TriviaResponseDto>> {
     const pageOptionsDtoFallBack = createPageOptionFallBack(options);
     const { order, skip, numOfItemsPerPage } = pageOptionsDtoFallBack;
 
@@ -60,7 +88,18 @@ export class TriviaService {
       this.triviaRepo.countDocuments().exec(),
     ]);
 
-    const triviaResponse = allTrivias.map((trivia) => toTriviaResponse(trivia));
+    const triviaResponse = await Promise.all(
+      allTrivias.map(async (trivia) => {
+        if (this.hasTimeElapsed(trivia.createdAt, trivia.duration)) {
+          await this.triviaRepo
+            .findByIdAndUpdate(trivia._id, { status: 'expired' }, { new: true })
+            .exec();
+          trivia.status = TRIVIA_STATUS.EXPIRED;
+        }
+
+        return toTriviaResponse(trivia);
+      }),
+    );
 
     const pageMetaDto = new PageMetaDto({
       itemCount,
@@ -116,5 +155,165 @@ export class TriviaService {
     await this.triviaRepo.deleteOne({ _id: trivia._id }).exec();
 
     return { message: 'Trivia deleted successfully' };
+  }
+
+  async submitTriviaAnswer(
+    userId: string,
+    triviaId: string,
+    answerDto: AnswerDto,
+  ) {
+    const { githubRepoLink } = answerDto;
+
+    const [user, trivia] = await Promise.all([
+      this.userService.findUserById(userId),
+      this.triviaRepo.findById(triviaId).exec(),
+    ]);
+
+    if (!trivia) {
+      throw new NotFoundException(ERROR.TRIVIA_NOT_FOUND);
+    }
+
+    if (this.hasTimeElapsed(trivia.createdAt, trivia.duration)) {
+      throw new ConflictException(ERROR.TRIVIA_EXPIRED);
+    }
+
+    const existingSubmission = await this.submissionRepo
+      .findOne({
+        userId,
+        triviaId,
+      })
+      .exec();
+
+    if (existingSubmission) {
+      throw new ConflictException(ERROR.DUPLICATE_SUBMISSION);
+    }
+
+    await new this.submissionRepo({
+      userId,
+      triviaId,
+      githubRepoLink,
+    }).save();
+
+    return { message: 'Github repository link was submitted successfully' };
+  }
+
+  async getAllSubmissions(
+    options: PageOptionsDto,
+  ): Promise<PaginationResponseDto<{ id: string; status: SUBMISSION_STATUS }>> {
+    const pageOptionsDtoFallBack = createPageOptionFallBack(options);
+    const { order, skip, numOfItemsPerPage } = pageOptionsDtoFallBack;
+
+    if (order !== Order.ASC && order !== Order.DESC) {
+      throw new BadRequestException('Order must be either "asc" or "desc"');
+    }
+
+    const [allSubmission, itemCount] = await Promise.all([
+      this.submissionRepo
+        .find()
+        .sort({ createdAt: order === Order.ASC ? 1 : -1 })
+        .skip(skip)
+        .limit(numOfItemsPerPage)
+        .exec(),
+      this.submissionRepo.countDocuments().exec(),
+    ]);
+
+    const submissions = await Promise.all(
+      allSubmission.map(async (submission) => {
+        return toSubmissionResponse(submission);
+      }),
+    );
+
+    const pageMetaDto = new PageMetaDto({
+      itemCount,
+      pageOptionsDto: pageOptionsDtoFallBack,
+    });
+
+    return {
+      data: submissions,
+      pagination: pageMetaDto,
+    };
+  }
+
+  async approveAnswer(submissionId: string, review: REVIEW_STATUS) {
+    const submission = await this.submissionRepo.findById(submissionId).exec();
+
+    if (!submission) {
+      throw new NotFoundException(
+        `Submission with ID ${submissionId} was not found`,
+      );
+    }
+
+    if (
+      submission.status === SUBMISSION_STATUS.PASSED ||
+      submission.status === SUBMISSION_STATUS.FAILED
+    ) {
+      throw new ConflictException(
+        `Submission with ID ${submissionId} has already been reviewed`,
+      );
+    }
+
+    const trivia = await this.triviaRepo.findById(submission.triviaId).exec();
+
+    if (trivia.status === TRIVIA_STATUS.EXPIRED) {
+      throw new GoneException('This Trivia is no longer available');
+    }
+
+    if (trivia.winners === trivia.maxWinners) {
+      throw new ConflictException(`All winners have already been awarded`);
+    }
+
+    const updatedSubmission = await this.submissionRepo
+      .findOneAndUpdate(
+        { _id: submission._id },
+        {
+          status:
+            review === REVIEW_STATUS.PASSED
+              ? SUBMISSION_STATUS.PASSED
+              : SUBMISSION_STATUS.FAILED,
+        },
+        {
+          new: true,
+        },
+      )
+      .exec();
+
+    if (review === REVIEW_STATUS.PASSED) {
+      await this.awardAlgos(updatedSubmission);
+
+      await this.triviaRepo
+        .findOneAndUpdate(
+          { _id: trivia._id },
+          { $inc: { winners: 1 } },
+          { new: true },
+        )
+        .exec();
+    }
+
+    return {
+      message: 'Submission has been reviewed successfully',
+    };
+  }
+
+  async awardAlgos(submission: any) {
+    const trivia = await this.triviaRepo.findById(submission.triviaId).exec();
+
+    if (trivia?.prize) {
+      await this.userService.awardAlgos(submission.userId, trivia.prize);
+    }
+  }
+
+  async showLeaderboard(): Promise<LeaderboardResponseDto[]> {
+    const users = await this.userService.getAllUsers();
+
+    const leaderboard = users.map((user) => toLeaderBoard(user));
+
+    return leaderboard;
+  }
+
+  private hasTimeElapsed(createdAt: Date, duration: number): boolean {
+    const currentTime = new Date();
+    const expirationTime = new Date(createdAt.getTime() + duration * 1000);
+
+    return currentTime >= expirationTime;
   }
 }
